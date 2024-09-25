7d8b9cef60b5f7cac367b398266f213fce8cea01
lester <gabriel.vikstrom@gmail.com>
2024-09-25 10:50:13 +0200

    Use KeyValuePairs as well as tables

--- Interpret-grading-documents/Services/FormRecognizerService.cs
+++ Interpret-grading-documents/Services/FormRecognizerService.cs
@@ -1,7 +1,10 @@
 ﻿using Azure;
 using Azure.AI.FormRecognizer.DocumentAnalysis;
 using Azure.AI.FormRecognizer.Models;
+using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Interpret_grading_documents.Services
@@ -26,9 +29,9 @@ namespace Interpret_grading_documents.Services
 
             var keyValuePairs = new Dictionary<string, string>();
 
+            // 1. Extract key-value pairs from tables
             foreach (var table in result.Tables)
             {
-                //extractedText += $"--- Page {page.PageNumber} ---\n";
                 foreach (var cell in table.Cells)
                 {
                     if (cell.ColumnIndex == 0)
@@ -39,6 +42,21 @@ namespace Interpret_grading_documents.Services
                         {
                             keyValuePairs[key] = value;
                         }
+                    }
+                }
+            }
+
+            foreach (var documentField in result.KeyValuePairs)
+            {
+                var key = documentField.Key?.Content;
+                var value = documentField.Value?.Content;
+
+                if (!string.IsNullOrEmpty(key) && !string.IsNullOrEmpty(value))
+                {
+                    // avoid duplicate keys
+                    if (!keyValuePairs.ContainsKey(key))
+                    {
+                        keyValuePairs[key] = value;
                     }
                 }
             }
