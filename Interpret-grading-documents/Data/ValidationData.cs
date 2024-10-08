5402853375451480c86b77d6e2a512e5cd5bc238
Gabriel Vikström <gabriel.vikstrom@gmail.com>
2024-10-08 15:17:14 +0200

    Use pure methods

---
+++ Interpret-grading-documents/Data/ValidationData.cs
@@ -0,0 +1,28 @@
+﻿using System.Text.Json;
+
+namespace Interpret_grading_documents.Data
+{
+    public class CourseDetail
+    {
+        public string CourseName { get; set; }
+        public string CourseCode { get; set; }
+        public int? Points { get; set; }
+    }
+
+    public static class ValidationData
+    {
+        public static Dictionary<string, CourseDetail> GetCourses()
+        {
+            string validationJsonPath = Path.Combine("Data", "kurser.json");
+            string validationJson = File.ReadAllText(validationJsonPath);
+            var validationCourses = JsonSerializer.Deserialize<Dictionary<string, CourseDetail>>(validationJson);
+
+            // Assign CourseName to each CourseDetail (since the key is the course name)
+            foreach (var kvp in validationCourses)
+            {
+                kvp.Value.CourseName = kvp.Key;
+            }
+            return validationCourses;
+        }
+    }
+}
